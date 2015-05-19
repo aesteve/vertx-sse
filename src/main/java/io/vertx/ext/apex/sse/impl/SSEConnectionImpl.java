c4ba75789cfd8f8a28539cd47e04f7178aa9d0c6
aesteve <aesteve@evenium.com>
2015-05-19 18:16:25 +0200

    change interface to be codegen-friendly
    still not sure about method overloading though

--- src/main/java/io/vertx/ext/apex/sse/impl/SSEConnectionImpl.java
+++ src/main/java/io/vertx/ext/apex/sse/impl/SSEConnectionImpl.java
@@ -1,5 +1,7 @@
 package io.vertx.ext.apex.sse.impl;
 
+import java.util.List;
+
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.apex.RoutingContext;
@@ -16,77 +18,113 @@ public class SSEConnectionImpl implements SSEConnection {
 
     @Override
     public SSEConnection reject(int code) {
-    	return reject(code, null);
+        return reject(code, null);
     }
-    
+
     @Override
     public SSEConnection reject(int code, String reason) {
-    	rejected = true;
-    	HttpServerResponse response = context.response();
-    	response.setStatusCode(code);
-    	if (reason != null) {
-    		response.setStatusMessage(reason);
-    	}
-    	response.end();
-    	return this;
-    }
-    
-    @Override
-    public SSEConnection comment(String comment) {
-    	context.response().write("comment: "+comment+"\n\n");
-    	return this;
-    }
-    
-    @Override
-    public SSEConnection retry(Long delay, String... data) {
-    	return withHeader("delay", delay.toString(), data);
-    }
-    
-    @Override
-    public SSEConnection data(String... data) {
-        appendData(data);
+        rejected = true;
+        HttpServerResponse response = context.response();
+        response.setStatusCode(code);
+        if (reason != null) {
+            response.setStatusMessage(reason);
+        }
+        response.end();
         return this;
     }
 
     @Override
-    public SSEConnection event(String eventName, String... data) {
+    public SSEConnection comment(String comment) {
+        context.response().write("comment: " + comment + "\n\n");
+        return this;
+    }
+
+    @Override
+    public SSEConnection retry(Long delay, List<String> data) {
+        return withHeader("delay", delay.toString(), data);
+    }
+
+    @Override
+    public SSEConnection retry(Long delay, String data) {
+        return withHeader("delay", delay.toString(), data);
+    }
+
+    @Override
+    public SSEConnection data(List<String> data) {
+        return appendData(data);
+    }
+
+    @Override
+    public SSEConnection data(String data) {
+        return writeData(data);
+    }
+
+    @Override
+    public SSEConnection event(String eventName, List<String> data) {
         return withHeader("event", eventName, data);
     }
-    
+
     @Override
-    public SSEConnection id(String id, String... data) {
-    	return withHeader("id", id, data);
+    public SSEConnection event(String eventName, String data) {
+        return withHeader("event", eventName, data);
+    }
+
+    @Override
+    public SSEConnection id(String id, List<String> data) {
+        return withHeader("id", id, data);
+    }
+
+    @Override
+    public SSEConnection id(String id, String data) {
+        return withHeader("id", id, data);
     }
 
     @Override
     public SSEConnection close() {
-    	context.response().close();
+        context.response().close();
         return this;
     }
 
     @Override
     public HttpServerRequest request() {
-    	return context.request();
+        return context.request();
     }
-    
+
     @Override
     public boolean rejected() {
-    	return rejected;
+        return rejected;
     }
-    
-    private SSEConnection withHeader(String headerName, String headerValue, String... data) {
-    	HttpServerResponse response = context.response();
-    	response.write(headerName + ": " + headerValue + "\n");
-    	return this;
+
+    private SSEConnection withHeader(String headerName, String headerValue, String data) {
+        writeHeader(headerName, headerValue);
+        writeData(data);
+        return this;
     }
-    
-    private void appendData(String... data) {
-        for (int i = 0; i < data.length; i++) {
-        	String separator = "\n";
-        	if (i == data.length - 1) {
-        		separator += "\n";
-        	}
-            context.response().write("data: " + data[i] + separator);
+
+    private SSEConnection withHeader(String headerName, String headerValue, List<String> data) {
+        writeHeader(headerName, headerValue);
+        appendData(data);
+        return this;
+    }
+
+    private SSEConnection writeHeader(String headerName, String headerValue) {
+        context.response().write(headerName + ": " + headerValue + "\n");
+        return this;
+    }
+
+    private SSEConnection writeData(String data) {
+        context.response().write("data: " + data + "\n\n");
+        return this;
+    }
+
+    private SSEConnection appendData(List<String> data) {
+        for (int i = 0; i < data.size(); i++) {
+            String separator = "\n";
+            if (i == data.size() - 1) {
+                separator += "\n";
+            }
+            context.response().write("data: " + data.get(i) + separator);
         }
+        return this;
     }
 }
