a0fc8cf666275ddd39b935495fc676e1f2ccedb8
Arnaud ESTEVE <arnaud.esteve@decathlon.com>
2017-06-08 10:39:12 +0200

    test bridge

--- src/test/java/io/vertx/ext/web/handler/sse/TestEventBusBridgeMapped.java
+++ src/test/java/io/vertx/ext/web/handler/sse/TestEventBusBridgeMapped.java
@@ -8,6 +8,9 @@ import io.vertx.ext.web.Router;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 @RunWith(VertxUnitRunner.class)
 public class TestEventBusBridgeMapped extends TestBase {
 
@@ -23,9 +26,9 @@ public class TestEventBusBridgeMapped extends TestBase {
 	@Test
 	public void testBridge(TestContext ctx) {
 		Async async = ctx.async();
-		final String id = "someaddress";
-		final String url = "/bridge/mapped/" + id;
-		final String message = "sent over the event bus, on a mapped address";
+		String id = "someaddress";
+		String url = "/bridge/mapped/" + id;
+		String message = "sent over the event bus, on a mapped address";
 		eventSource().connect(url, res -> {
 			ctx.assertFalse(res.failed());
 			vertx.eventBus().publish("anotheraddress", "should not be received");
@@ -34,6 +37,33 @@ public class TestEventBusBridgeMapped extends TestBase {
 			ctx.assertEquals(message + "\n", msg);
 			async.complete();
 		});
+	}
+
+
+	@Test
+	public void testTwoConnections(TestContext ctx) throws Exception {
+		String id1 = "someaddress";
+		String id2 = "anotheraddress";
+		String url1 = "/bridge/mapped/" + id1;
+		String url2 = "/bridge/mapped/" + id2;
+		String message1 = "sent over the event bus, on address " + id1;
+		String message2 = "sent over the event bus, on address " + id2;
+		CountDownLatch latch = new CountDownLatch(2);
+		eventSource().connect(url1, res -> {
+			ctx.assertFalse(res.failed());
+			eventSource().connect(url2, res2 -> {
+				ctx.assertFalse(res2.failed());
+				vertx.eventBus().publish(id2, message2);
+			}).onMessage(msg -> {
+				ctx.assertEquals(message2 + "\n", msg);
+				latch.countDown();
+			});
+			vertx.eventBus().publish(id1, message1);
+		}).onMessage(msg -> {
+			ctx.assertEquals(message1 + "\n", msg);
+			latch.countDown();
+		});
+		latch.await(1, TimeUnit.SECONDS);
 	}
 
 }
